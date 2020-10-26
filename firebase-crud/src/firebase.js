d165ac3c1deac5ef83c6092bb39b8b33a19091a4
Akshay phalphale <phalphaleakshay@gmail.com>
2020-10-26 16:37:29 +0530

    Update firebase.js
    remove credentials from firebase config file.

--- firebase-crud/src/firebase.js
+++ firebase-crud/src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCQErWYtZxFl09_eJub5V_863DM6Q70gsU",
-    authDomain: "contacts-f7351.firebaseapp.com",
-    databaseURL: "https://contacts-f7351.firebaseio.com",
-    projectId: "contacts-f7351",
-    storageBucket: "contacts-f7351.appspot.com",
-    messagingSenderId: "43251766165",
-    appId: "1:43251766165:web:f29292429dd81a8ea01c9c"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 const fireDb = firebase.initializeApp(firebaseConfig);
